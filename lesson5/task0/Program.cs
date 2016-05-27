8103aa58622772ecd86ad713f44e6447eacb1199
SStasL <stas.shmulich@gmail.com>
2016-05-27 14:38:01 +0300

    i am adding to my proj standard dictionary from example

--- lesson5/task0/Program.cs
+++ lesson5/task0/Program.cs
@@ -10,6 +10,25 @@ namespace task0
     {
         static void Main(string[] args)
         {
+            Dictionary dictionary = new Dictionary();
+
+            Console.WriteLine(dictionary["книга"]);
+            Console.WriteLine(dictionary["дом"]);
+            Console.WriteLine(dictionary["ручка"]);
+            Console.WriteLine(dictionary["стол"]);
+            Console.WriteLine(dictionary["карандаш"]);
+            Console.WriteLine(dictionary["яблоко"]);
+            Console.WriteLine(dictionary["солнце"]);
+
+            Console.WriteLine(new string('-', 20));
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(dictionary[i]);
+            }
+
+            // Delay.
+            Console.ReadKey();
         }
     }
 }
